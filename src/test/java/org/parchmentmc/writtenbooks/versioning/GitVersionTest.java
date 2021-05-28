8e9044bd24d89e0b374779fba4de05903aee44f5
Marc Hermans <marc.hermans@ldtteam.com>
2021-05-28 18:24:56 +0200

    Fix the unit tests.

--- src/test/java/org/parchmentmc/writtenbooks/versioning/GitVersionTest.java
+++ src/test/java/org/parchmentmc/writtenbooks/versioning/GitVersionTest.java
@@ -12,24 +12,24 @@ public class GitVersionTest {
     public void testVersionStrings() {
         assertEquals("1.0.0",
                 createVersionString("1.0.0", 0, "main", DEFAULT_MAIN_BRANCHES::contains));
-        assertEquals("1.0.0-SNAPSHOT",
+        assertEquals("1.0.0.25-SNAPSHOT",
                 createVersionString("1.0.0", 25, "main", DEFAULT_MAIN_BRANCHES::contains));
         assertEquals("1.0.0",
                 createVersionString("1.0.0", 0, "dev", DEFAULT_MAIN_BRANCHES::contains));
-        assertEquals("1.0.0-dev-SNAPSHOT",
+        assertEquals("1.0.0.25-dev-SNAPSHOT",
                 createVersionString("1.0.0", 25, "dev", DEFAULT_MAIN_BRANCHES::contains));
-        assertEquals("1.0.0-main-SNAPSHOT",
+        assertEquals("1.0.0.25-main-SNAPSHOT",
                 createVersionString("1.0.0", 25, "main", s -> false));
 
         assertEquals("1.1.0",
                 createVersionString("v1.1.0", 0, "main", DEFAULT_MAIN_BRANCHES::contains));
-        assertEquals("1.1.0-SNAPSHOT",
+        assertEquals("1.1.0.25-SNAPSHOT",
                 createVersionString("v1.1.0", 25, "main", DEFAULT_MAIN_BRANCHES::contains));
         assertEquals("1.1.0",
                 createVersionString("v1.1.0", 0, "dev", DEFAULT_MAIN_BRANCHES::contains));
-        assertEquals("1.1.0-dev-SNAPSHOT",
+        assertEquals("1.1.0.25-dev-SNAPSHOT",
                 createVersionString("v1.1.0", 25, "dev", DEFAULT_MAIN_BRANCHES::contains));
-        assertEquals("1.1.0-main-SNAPSHOT",
+        assertEquals("1.1.0.25-main-SNAPSHOT",
                 createVersionString("v1.1.0", 25, "main", s -> false));
     }
 }
